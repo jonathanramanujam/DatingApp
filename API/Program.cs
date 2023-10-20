9f39783bb749a319a8c4de08af2ccdb632f81b0e
jonathanramanujam <jon.ramanujam@gmail.com>
2023-10-19 19:17:13 -0600

    mkcert
    Added mkcert to sign digital certificates and utilize https rather than http.

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 // This will allow connection via our Angular Development Server which runs on localhost:4200
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
