a1e25db268480b4dd87affdda9a0d5220472c77d
jonathanramanujam <jon.ramanujam@gmail.com>
2023-10-23 16:27:49 -0600

    Added Register DTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API;
 
@@ -15,14 +17,18 @@ public class AccountController : BaseApiController
     }
 
     [HttpPost("register")] // POST api/account/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await UserExists(registerDto.Username))
+        {
+            return BadRequest("Username is taken.");
+        }
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -30,6 +36,12 @@ public class AccountController : BaseApiController
         await _context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        // If there is a user who has this username, assign to x. AnyAsync() will return T/F
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
 
 }
