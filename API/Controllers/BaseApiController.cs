1c469e95cbd9ac3c033752130f4edc6f4d176dd8
jonathanramanujam <jon.ramanujam@gmail.com>
2023-10-23 18:05:43 -0600

    RegisterDto Required Field Validation

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,6 +3,7 @@
 namespace API;
 
 // Base API Controller
+// Using the ApiController attribute offers functions like validation
 [ApiController]
 [Route("api/[controller]")]
 public class BaseApiController : ControllerBase
