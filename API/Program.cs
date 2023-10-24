e5bdd87f9045ad552fc391b1f4d1ee7fe827b5d9
jonathanramanujam <jon.ramanujam@gmail.com>
2023-10-23 20:30:49 -0600

    Added Authentication to API Methods

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,9 @@ using API.Data;
 using API.Services;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,12 +21,28 @@ builder.Services.AddCors(); // Cors == Cross-Origin Resource Sharing. Protects b
 // AddScoped makes most sense here, because AddTransient would be created/deleted for each use,
 // and AddSingleton would live in memory, consuming resources.
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding
+            .UTF8.GetBytes(builder.Configuration["TokenKey"])),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 // This will allow connection via our Angular Development Server which runs on localhost:4200
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication(); // Do you have a valid token?
+app.UseAuthorization(); // What are you allowed to do?
 
 app.MapControllers();
 
