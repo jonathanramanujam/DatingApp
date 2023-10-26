1d59e42e0fff9ae29cb5a0bdb3647dffadde9b18
Jonathan Ramanujam <75343684+jonathanramanujam@users.noreply.github.com>
2023-10-26 13:44:47 -0600

    currentUser$ observable now checks logged in

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -9,30 +11,20 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit
 {
   model: any = {}
-  loggedIn = false;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void
   {
-    this.getCurrentUser();
-  }
-
-  getCurrentUser()
-  {
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    });
   }
 
   login()
   {
+    // Http requests do not need to be unsubscribed from, as http requests complete
     this.accountService.login(this.model).subscribe({
       next: response =>
       {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
@@ -41,6 +33,5 @@ export class NavComponent implements OnInit
   logout()
   {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 }
