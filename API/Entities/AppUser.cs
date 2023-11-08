0d44c458b84b517d35cf05be54e4b7728c26fa81
Jonathan Ramanujam <75343684+jonathanramanujam@users.noreply.github.com>
2023-11-08 15:51:56 -0700

    Photo Class, Age Extension
    Added the photo class and created a new db migration. The age extension was also added to calculate user age.

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -17,4 +17,9 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> photos { get; set; } = new();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,9 +1,16 @@
-﻿namespace API.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace API.Entities;
+
+// This Attribute allows us to pluralize the table name,
+// without creating a dbset, since photos are always tied to a particular user.
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
     public string Url { get; set; }
     public bool IsMain { get; set; }
     public string PublicId { get; set; }
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
 }
