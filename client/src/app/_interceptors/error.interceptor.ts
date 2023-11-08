f85b73ccee9b5588c2b0ad97159bcb3d7bbce134
Jonathan Ramanujam <75343684+jonathanramanujam@users.noreply.github.com>
2023-11-07 20:02:21 -0700

    Display array of registration/login form errors
    400ValidationError updated to pack all errors into a single array

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -35,7 +35,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                     modelStateErrors.push(error.error.errors[key]);
                   }
                 }
-                throw modelStateErrors;
+                throw modelStateErrors.flat();
               }
               else
               {
--- client/src/app/errors/test-error/test-error.component.ts
+++ client/src/app/errors/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class TestErrorComponent
 {
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -48,7 +49,11 @@ export class TestErrorComponent
   {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error =>
+      {
+        console.log(error),
+          this.validationErrors = error
+      }
     });
   }
 }
