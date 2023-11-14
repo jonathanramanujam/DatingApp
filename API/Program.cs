b873104e4534a87de35e7a40bc8aeb4c6bb7deb3
jonathanramanujam <jon.ramanujam@gmail.com>
2023-11-14 11:26:26 -0700

    Seeding Users
    The program now checks to see if the database has users, and if not, it will seed the database with dummy users. Ann is the first username, and "Pa$$w0rd" is the pass.

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API;
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +25,18 @@ app.UseAuthentication(); // Do you have a valid token?
 app.UseAuthorization(); // What are you allowed to do?
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
